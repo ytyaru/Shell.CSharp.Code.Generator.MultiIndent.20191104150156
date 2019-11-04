0daedcc9022e5af823af0ec4b01c9bafaf2f5a2f
ytyaru <ytyaru@outlook.jp>
2019-11-04 15:05:04 +0900

    初回コミット

---
+++ src/indent.sh
@@ -0,0 +1,24 @@
+# stdin: インデントしたいテキスト  $1: インデント用文字列（任意。省略時はタブ）
+indent() { echo "$(cat -)" | sed "s/^/${1:-\t}/g"; }
+# $1..9..{10}.. 位置引数の位置がそのままインデント階層である
+indent_pos() {
+	local result=""
+	local indent=0
+	# $1: 繰り返したいテキスト（省略時""）  $2: 回数（省略時1）
+	repeat() {
+		local target=${1:-}; local count=${2:-2}; let count--;
+		[ 0 -lt $count ] && printf "$target"'%*s' $count;
+	}
+	# ソフトインデントを返す。$1: インデント数(省略時0) $2: 1インデントあたりのスペース数(省略時4)  
+	soft_indent() { repeat ' ' $((${1:-0}* ${2:-4})); }
+	# $1を$2だけインデントする
+	indent_part() {
+		[ -z "${2}" -o 0 -eq ${2} ] && echo "$1" || \
+			echo "${1}" | indent "$(soft_indent $indent)";
+	}
+	for code in "$@"; do
+		result+="$(indent_part "$code" $indent)""\n"
+		let indent++
+	done
+	echo -e "$result" | head -c -1
+}
---
+++ src/run.sh
@@ -0,0 +1,3 @@
+. indent.sh
+indent_pos 'namespace MyProj' 'class MyClass' 'static void Main()' "$(echo -e "int i;\ni = 5;")"
+
